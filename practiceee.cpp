788052220cd59313842dc7623f5dfb5a60cb9220
shariar-opu <126817974+shariar-opu@users.noreply.github.com>
2024-12-26 02:03:28 +0600

    ok

--- practiceee.cpp
+++ practiceee.cpp
@@ -1,5 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    ;
+    string s;   cin >> s;
+    int l = 0, r = 0;
+    map<char, int> mp;
+    int cnt = 0, ans = 1;
+    while(l < s.size() && r < s.size()){
+        mp[s[r]]++;
+        if(mp[s[r]] > 1){
+            r = l+1;
+            l = r;
+            cnt = 0;
+            mp.clear();
+        }
+        else {
+            r++;
+            cnt++;
+        }
+        ans = max(ans, cnt);
+    }
+    cout << ans << endl;
 }
