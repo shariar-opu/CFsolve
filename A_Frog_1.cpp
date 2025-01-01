b2c6547ed11585c07acb5b52721f9c19100fb322
shariar-opu <126817974+shariar-opu@users.noreply.github.com>
2025-01-01 18:13:12 +0600

    ok

--- A_Frog_1.cpp
+++ A_Frog_1.cpp
@@ -1,25 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int inf = 1e5 + 7;
-int n;
-int arr[inf], dp[inf];
+int n, arr[inf], dp[inf];
 int solve(int idx){
-    if(idx == n - 1) return arr[n-1];
-    if(idx == n - 2) return abs(arr[n-1] - arr[n-2]);
+    if(idx == n) return 0;
     if(dp[idx]) return dp[idx];
 
     int res = INT_MAX;
     for(int i = 1; i <= 2; i++){
-        res = min(res, abs(arr[idx] - arr[idx+i]) + solve(idx+i));
+        if(idx + i <= n) res = min(res, abs(arr[idx] - arr[idx+i]) + solve(idx+i));
     }
     dp[idx] = res;
     return dp[idx];
 }
 int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     cin >> n;
-    for(int i = 0; i < n; i++){
+    for(int i = 1; i <= n; i++){
         cin >> arr[i];
     }
-    int ans = solve(0);
+    int ans = solve(1);
     cout << ans << endl;
 }
