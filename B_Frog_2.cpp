0a7a5b147c77c0a803d5183bdc7eee5a768ce996
shariar-opu <126817974+shariar-opu@users.noreply.github.com>
2025-01-08 17:33:37 +0600

    ok

--- B_Frog_2.cpp
+++ B_Frog_2.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int inf = 1e5 + 5;
-int n, arr[inf], dp[inf], vis[inf];
+int n, k, arr[inf], dp[inf], vis[inf];
 int solve(int idx){
     if(idx == n) return 0;
     if(vis[idx]) return dp[idx];
 
     int res = INT_MAX;
-    for(int i = 1; i <= 2; i++){
+    for(int i = 1; i <= k; i++){
         if(idx + i <= n){
             int cost = abs(arr[idx] - arr[idx+i]) + solve(idx+i);
             res = min(res, cost);
@@ -20,7 +20,7 @@ int solve(int idx){
 }
 int main(){
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    cin >> n;
+    cin >> n >> k;
     for(int i = 1; i <= n; i++){
         cin >> arr[i];
     }
--- B_Maximum_Sum.cpp
+++ B_Maximum_Sum.cpp
@@ -3,31 +3,27 @@ using namespace std;
 #define ll long long
 
 void solve(int tc){
-  int n, k; cin >> n >> k;
-  vector<ll> arr(n);
-  for(ll &i : arr) cin >> i;
-
-  sort(arr.begin(), arr.end());
-
-  ll sum = accumulate(arr.begin(), arr.end(), 1ll*0);
-  ll l1 = 0, r1 = 1, r11 = n - 2, mx = sum - arr[n-1];
-  ll l2 = 2, r2 = 3, r22 = n - 1, mn = sum - arr[0] - arr[1];
-
-  for(int i = 1; i < k; i++){
-    mx -= min(arr[l1] + arr[r1], arr[r11]);
-    if(arr[l1] + arr[r1] >= arr[r11]) r11--;
-    else l1 = r1 + 1, r1 = l1 + 1;
-    mn -= min(arr[l2] + arr[r2], arr[r22]);
-    if(arr[l2] + arr[r2] >= arr[r22]) r22--;
-    else l2 = r2 + 1, r2 = l2 + 1;
+  int n, k;   cin >> n >> k;
+  vector<ll> arr(n + 1, 0), pre(n + 1, 0);
+  for(int i = 1; i <= n; i++){
+    cin >> arr[i];
   }
-
-  cout << max(mn, mx) << endl;
+  sort(arr.begin(), arr.end());
+  for(int i = 1; i <= n; i++){
+    pre[i] = pre[i-1] + arr[i];
+  }
+  
+  ll ans = 0;
+  for(int i = 0; i <= k; i++){
+    ans = max(ans, pre[n - (k - i)] - pre[2 * i]);
+  }
+  cout << ans << endl;
 }
 
-int32_t main(){
+int main(){
   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-  int t = 1;  cin >> t;
+  int t = 1;
+  cin >> t;
   for(int i = 1; i <= t; i++) solve(i);
-  
+  return 0;
 }
