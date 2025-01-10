846fadb60ab75d011b52485eff41486aa4f10e33
shariar-opu <126817974+shariar-opu@users.noreply.github.com>
2025-01-11 03:06:35 +0600

    ok

--- B_Kalindrome_Array.cpp
+++ B_Kalindrome_Array.cpp
@@ -1,21 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long
+
+void solve(){
+    int n;  cin >> n;
+    vector<int> arr(n);
+    for(int &i : arr) cin >> i;
+
+    auto check = [&](int a){
+        int l = 0, r = n-1;
+        while(r - l > 0){
+            if(arr[l] == a) l++;
+            else if(arr[r] == a) r--;
+            else{
+                if(arr[l] != arr[r]) return 0;
+                l++, r--;
+            }
+        }
+        return 1;
+    };
+
+    int a, b, ans = -1, l = 0, r = n-1;
+    while(r - l> 0){
+        if(arr[l] != arr[r]){
+            a = arr[l], b = arr[r];
+            ans = 0;
+            break;
+        }
+        l ++, r --;
+    }
+    
+    if(ans == -1) cout << "YES\n";
+    else {
+        if(check(a) || check(b)) cout << "YES\n";
+        else cout << "NO\n";
+    }
+}
+
 int main(){
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int t;  cin >> t;
     while(t--){
-        int n;  cin >> n;
-        vector<int> arr(n);
-        map<int, int> mp;
-        for(int &i : arr){
-            cin >> i;
-            mp[i]++;
-        }
-        int cnt = 0;
-        for(auto u : mp){
-            if(u.second == 1) cnt++;
-        }
-        if(cnt > 2) cout << "NO\n";
-        else cout << "YES\n";
+        solve();
     }
-}
+    return 0;
+} 
