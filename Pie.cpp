e3ac7738e00a7d06d3383c28f93a15a301e518bb
shariar-opu <126817974+shariar-opu@users.noreply.github.com>
2025-01-18 17:02:50 +0600

    ok

--- Pie.cpp
+++ Pie.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-const double pi = 3.1416;
-const double inf = 2e-9;
+const double pi = 3.14159265358979323; //normally 11 ta digit rakhle accept hoy.
+const double inf = 1e-6;
 int n, f;
 vector<double> pie;
 
@@ -12,16 +12,17 @@ bool mono(double dist){
     }
     return eater <= portion;
 }
+
 double bs(){
-    double l = 1, r = 1e9;
+    double l = 0, r = 1e9;
     while(r - l > inf){
-        double mid = (r+l)/2;
+        double mid = (r+l)/2.0;
         if(mono(mid)) l = mid;
         else r = mid;
     }
-    if(mono(r)) return r;
     return l;
 }
+
 void solve(int tc){
     cin >> n >> f;
     pie.resize(n);
@@ -29,12 +30,16 @@ void solve(int tc){
         double a;  cin >> a;
         pie[i] = pi * a * a;
     }
-    // for(auto i : pie) cout << i << endl;
+
     cout << fixed << setprecision(4) << bs() << endl;
 }
+
 int main(){
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
     int t = 1; 
     cin >> t;
-    while(t--) solve(t);
+    for(int i = 1; i <= t; i++) solve(i);
+
+    return 0;
 }
