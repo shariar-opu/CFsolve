f5d8a5cc74f40477e82afc3914636b085bcb3080
shariar-opu <126817974+shariar-opu@users.noreply.github.com>
2025-01-22 02:18:57 +0600

    ok

--- D_Knapsack_1.cpp
+++ D_Knapsack_1.cpp
@@ -3,39 +3,29 @@ using namespace std;
 #define ll long long
 
 const int inf = 1e5;
-ll dp[inf + 7], n;
+ll dp[107][inf + 7], n;
 vector<pair<ll, ll>> vp;
 
-ll solve(int w, int j){
+ll solve(int j, int w){
     if(w == 0) return 0;
-    if(dp[w]) return dp[w];
+    if(j < 0) return 0;
+    if(dp[j][w]) return dp[j][w];
 
-    ll res = 0;
-
-    for(int i = 0; i < n; i++){
-        if(w - vp[i].first >= 0 && i != j) {
-            res = max(res, vp[i].second + solve(w - vp[i].first, i));
-        }
-    }
-
-    return dp[w] = res;
+    ll res = solve(j - 1, w);
+    if(w >= vp[j].first) res = max(res, solve(j - 1, w - vp[j].first) + vp[j].second);
+    
+    return dp[j][w] = res;
 }
 
 int main(){
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
     int w;  cin >> n >> w;
     for(int i = 0; i < n; i++) {
         ll a, b;   cin >> a >> b;
         vp.push_back({a, b});
     }
 
-    // sort(vp.begin(), vp.end(), [](pair<ll, ll> a, pair<ll, ll> b){
-    //     if(a.second == b.second) return a.first < b.first;
-    //     else return a.second > b.second;
-    // });
-
-    // for(auto u : vp) cout << u.first << " " << u.second << endl;
-
-    cout << solve(w, 0) << endl;
+    cout << solve(n - 1, w) << endl;
     return 0;
 }
