6c0e4afb3c62df96c20800a449a41d8945d31b92
shariar-opu <126817974+shariar-opu@users.noreply.github.com>
2025-02-22 16:04:08 +0600

    ok

--- E_Concert_Tickets.cpp
+++ E_Concert_Tickets.cpp
@@ -1,38 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-const int inf = 2e5;
-int n, m; 
-vector<int> price_tic, cus_price;
-bool  mark[inf + 7];
-
-bool check(int x){
-    for(int i = 0; i < )
-}
-
-int bs(){
-    long long l = 0, r = 1e9, ans = -1;
-    while(r - l > 0){
-        long long mid = (r + l) / 2;
-        if(check(mid)) {
-            ans = mid;
-            l = mid + 1;
-        }
-        else r = mid - 1;
-    }
-    return ans;
-}
+#define ll long long
 
 int main(){
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    
+    int n, m;   cin >> n >> m;
 
-    cin >> n >> m;
-    price_tic.resize(n), cus_price.resize(m);
+    multiset<ll> price_tic;
+    for(int i = 0; i < n; i++){
+        ll a;   cin >> a;
+        price_tic.insert(a);
+    }
 
-    for(int &i : price_tic) cin >> i;
-    for(int &i : cus_price) cin >> i;
+    for(int i = 0; i < m; i++){
+        ll cus_price;   cin >> cus_price;
 
-    sort(price_tic.begin(), price_tic.end());
+        auto it = price_tic.upper_bound(cus_price);
 
-    cout << bs() << endl;
+        if(it == price_tic.begin()){
+            cout << -1 << endl;
+        }
+        else{
+            --it;
+            cout << *it << endl;
+            price_tic.erase(it);
+        }
+    }
 }
