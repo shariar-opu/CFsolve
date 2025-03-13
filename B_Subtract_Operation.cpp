03ec52aa02e8c42a804fa91e42f308e9f52ca2ee
shariar-opu <126817974+shariar-opu@users.noreply.github.com>
2025-03-14 04:02:10 +0600

    ok

--- B_Subtract_Operation.cpp
+++ B_Subtract_Operation.cpp
@@ -1,37 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define int long long
 
 void solve(){
     int n, k;  cin >> n >> k;
-    priority_queue<int, vector<int>, greater<int>> pq;
-    for(int i = 0; i < n; i++){
-        int a;  cin >> a;
-        pq.push(a);
+    vector<int> arr(n);
+    for(int &i : arr) cin >> i;
+
+    sort(arr.begin(), arr.end());
+
+    int l = 0, r = 1;
+    while(r < n){
+        // cout << arr[r] << " " << arr[l] << " " << arr[r] - arr[l] << endl;
+        if(arr[r] - arr[l] == k){
+            cout << "YES\n";
+            return;
+        }
+        else if(arr[r] - arr[l] > k) l++;
+        else r++;
     }
 
-    int sub = 0, cnt = 0;
-    while(!pq.empty()){
-        int x = pq.top();
-        x -= sub;
-        sub += x;
-        cout << pq.top() << " " << x << " " << sub << endl;
-        pq.pop();
-
-        // if(x == 0){
-        //     cnt++;
-        //     pq.pop();
-        // }
-        // else {
-        //     for(int i = 0; i < cnt; i++){
-        //         pq.push(-1 * x);
-        //     }
-        // }
-    }
-    cout << sub << endl;
-    cout << endl;
+    cout << "NO\n";
 }
 
-int main(){
+int32_t main(){
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
     int t;  cin >> t;
