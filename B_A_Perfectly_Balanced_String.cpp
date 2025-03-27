5062bb237942f2ed6c8a2be8cc3eb42e83409ba9
shariar-opu <126817974+shariar-opu@users.noreply.github.com>
2025-03-27 17:54:52 +0600

    ok

--- B_A_Perfectly_Balanced_String.cpp
+++ B_A_Perfectly_Balanced_String.cpp
@@ -2,59 +2,35 @@
 using namespace std;
 
 void solve(){
-    string s;   cin >> s;
-    vector<int> freq(26);
+    string s, tmp = "";   cin >> s;
+    vector<int> freq(26, 0);
 
-    //frequency count
-    for(auto u : s) freq[u - 'a']++;
-
-    //total charecter count
-    int tot_ch = 0;
-    for(int i = 0; i < 26; i++){
-        if(freq[i]) tot_ch++;
-    }
-
-    //base case
-    if(tot_ch == 1){
-        cout << "YES\n";
-        return;
-    }
-
-    //brute force solution
-    /*
-    map<string, vector<int>> mp;
-
-    for(int i = 0; i < s.size(); i++){
-        for(int j = i; j < s.size(); j++){
-            string tmp = s.substr(i, j + 1);
-            map<char, int> fre;
-            
-            for(auto u : tmp) fre[u]++;
-            vector<int> ch_fre(tot_ch, 0);
-
-            int serial = 0;
-            for(auto u : fre) {
-                ch_fre[serial] = u.second; 
-                serial++;
+    bool ok = 0;
+    int i = 0;
+    for(auto u : s){
+        if(!freq[u - 'a']){
+            if(!ok) tmp += u;
+            else {
+                cout << "NO\n";
+                return;
             }
-
-            mp[tmp] = ch_fre;
         }
-    }
-
-    for (auto &p : mp) {
-        cout << "\"" << p.first << "\" -> {";
-        for (int i = 0; i < tot_ch; i++) {
-            cout << p.second[i] << (i < tot_ch - 1 ? ", " : "");
+        else {
+            ok = 1;
+            if(u == tmp[i]){
+                i++;
+                if(tmp.size() <= i) i = 0;
+            }
+            else {
+                cout << "NO\n";
+                return;
+            }
         }
-        cout << "}\n";
+        freq[u - 'a']++;
     }
-    */
+    cout << "YES\n";
 
-    //optimezed solution
-
-    ;
-
+    // for(int i = 0; i < 26; i++) cout << i << " " << freq[i] << endl;
 }
 
 int main(){
