824dcbec9825aa8183035180b2c7b3823488d3d5
shariar-opu <126817974+shariar-opu@users.noreply.github.com>
2025-03-30 03:26:38 +0600

    ok

--- Coin_Combinations_II.cpp
+++ Coin_Combinations_II.cpp
@@ -1,22 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 const int mod = 1e9 + 7;
+
 int main(){
-    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ios_base::sync_with_stdio(0);cin.tie(nullptr);cout.tie(nullptr);
+    int n, price;     
+    cin >> n >> price;
 
-    int n, price;     cin >> n >> price;
-    vector<int> coins(n);
-    for(int i = 0; i < n; i++) cin >> coins[i];
-    vector<long long> dp(price + 1, 0);
+    vector<int> arr(n);
+    for(int &i : arr) cin >> i;
+
+    vector<int> dp(price + 7, 0);
     dp[0] = 1;
-
-    for(int i = 0; i < n; i++){
+    for(auto c : arr) {
         for(int val = 1; val <= price; val++){
-            if(val - coins[i] >= 0){
-                dp[val] = (dp[val] + dp[val - coins[i]]) % mod;
-            }
+            if(val - c >= 0) dp[val] = (dp[val] + dp[val - c]) % mod;
         }
     }
 
-    cout << dp[price];
+    cout << dp[price] << endl; 
 }
