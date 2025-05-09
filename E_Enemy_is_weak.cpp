94ba1ecf3b786db29a3abdcb68793a59d25eb3a7
shariar-opu <126817974+shariar-opu@users.noreply.github.com>
2025-05-10 04:03:46 +0600

    ok

--- E_Enemy_is_weak.cpp
+++ E_Enemy_is_weak.cpp
@@ -6,19 +6,41 @@ using namespace __gnu_pbds;
 using namespace std;
 
 #define ll long long
-template<typename T> using ordered_set = tree<T, null_type, greater<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> using os_less = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> using os_greater = tree<T, null_type, greater<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 void solve(int tc){
-    int n, ans = 0;  cin >> n;
+    int n;  cin >> n;
 
-    ordered_set<int> st;
-    for(int i = 0; i < n; i++){
-        int a;  cin >> a;
-        st.insert(a);
-        ans += st.order_of_key(a);
+    vector<ll> arr(n);
+    for(ll &i : arr) cin >> i;
+
+    os_less<ll> osl;
+    vector<ll> holdl(n);
+    for(int i = n - 1; i >= 0; --i){
+        osl.insert(arr[i]);
+        holdl[i] = osl.order_of_key(arr[i]);
     }
-    cout << ans - 2 << '\n';
 
+    os_greater<ll> osg;
+    vector<ll> holdg(n);
+    for(int i = 0; i < n; i++){
+        osg.insert(arr[i]);
+        holdg[i] = osg.order_of_key(arr[i]);
+    }
+
+    ll ans = 0;
+    bool ok = 0;
+    for(int i = 0; i < n - 1; i++){
+        if(ok){
+            ans += (holdg[i] * holdl[i]);
+        }
+        if(holdl[i] >= 2 && !ok){
+            ok = 1;
+        }
+    }
+
+    cout << ans << '\n';
 }
 
 int32_t main(){
